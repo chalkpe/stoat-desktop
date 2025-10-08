25506172f2a7f1d45b6ce19f1e1ef4a862b7933d
izzy <me@insrt.uk>
2025-10-08 21:01:06 +0100

    ci: separate build/publish (don't run build on all platforms) ci: don't try to build AppX in CI/CD context

--- forge.config.ts
+++ forge.config.ts
@@ -21,6 +21,107 @@ const STRINGS = {
 
 const ASSET_DIR = "assets/desktop";
 
+/**
+ * Build targets for the desktop app
+ */
+const makers: ForgeConfig["makers"] = [
+  new MakerSquirrel({
+    name: STRINGS.name,
+    authors: STRINGS.author,
+    // todo: hoist this
+    iconUrl: `https://stoat.chat/app/assets/icon-DUSNE-Pb.ico`,
+    // todo: loadingGif
+    setupIcon: `${ASSET_DIR}/icon.ico`,
+    description: STRINGS.description,
+    exe: `${STRINGS.execName}.exe`,
+    setupExe: `${STRINGS.execName}-setup.exe`,
+    copyright: "Copyright (C) 2025 Revolt Platforms LTD",
+  }),
+  new MakerZIP({}),
+];
+
+// skip these makers in CI/CD
+if (!process.env.PLATFORM) {
+  makers.push(
+    // must be manually built (freezes CI process)
+    // not much use in being published anyhow
+    new MakerAppX({
+      certPass: "",
+      packageExecutable: `app\\${STRINGS.execName}.exe`,
+      publisher: "CN=B040CC7E-0016-4AF5-957F-F8977A6CFA3B",
+    }),
+    // flatpak publishing should occur through flathub repos.
+    // this is just for testing purposes
+    new MakerFlatpak({
+      options: {
+        id: "chat.stoat.stoat-desktop",
+        description: STRINGS.description,
+        productName: STRINGS.name,
+        productDescription: STRINGS.description,
+        runtimeVersion: "21.08",
+        icon: `${ASSET_DIR}/icon.png`,
+        categories: ["Network"],
+        modules: [
+          // use the latest zypak -- Electron sandboxing for Flatpak
+          {
+            name: "zypak",
+            sources: [
+              {
+                type: "git",
+                url: "https://github.com/refi64/zypak",
+                tag: "v2025.09",
+              },
+            ],
+          },
+        ],
+        finishArgs: [
+          // default arguments found by running
+          // DEBUG=electron-installer-flatpak* pnpm make
+          "--socket=x11",
+          "--share=ipc",
+          "--device=dri",
+          "--socket=pulseaudio",
+          "--filesystem=home",
+          "--env=TMPDIR=/var/tmp",
+          "--share=network",
+          "--talk-name=org.freedesktop.Notifications",
+          // add Unity talk name for badges
+          "--talk-name=com.canonical.Unity",
+        ],
+        // files: [
+        //   // is this necessary?
+        //   // https://stackoverflow.com/q/79745700
+        //   ...[16, 32, 64, 128, 256, 512].map(
+        //     (size) =>
+        //       [
+        //         `assets/desktop/hicolor/${size}x${size}.png`,
+        //         `/app/share/icons/hicolor/${size}x${size}/apps/chat.stoat.stoat-desktop.png`,
+        //       ] as [string, string],
+        //   ),
+        //   [
+        //     `assets/desktop/icon.svg`,
+        //     `/app/share/icons/hicolor/scalable/apps/chat.stoat.stoat-desktop.svg`,
+        //   ] as [string, string],
+        // ],
+        files: [],
+      } as MakerFlatpakOptionsConfig,
+      /* as Omit<
+        MakerFlatpakOptionsConfig,
+        "files"
+      > */
+    }),
+    // testing purposes
+    new MakerDeb({
+      options: {
+        productName: STRINGS.name,
+        productDescription: STRINGS.description,
+        categories: ["Network"],
+        icon: `${ASSET_DIR}/icon.png`,
+      },
+    }),
+  );
+}
+
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
@@ -33,94 +134,7 @@ const config: ForgeConfig = {
     // ],
   },
   rebuildConfig: {},
-  makers: [
-    new MakerAppX({
-      certPass: "",
-      packageExecutable: `app\\${STRINGS.execName}.exe`,
-      publisher: "CN=B040CC7E-0016-4AF5-957F-F8977A6CFA3B",
-    }),
-    new MakerSquirrel({
-      name: STRINGS.name,
-      authors: STRINGS.author,
-      // todo: hoist this
-      iconUrl: `https://stoat.chat/app/assets/icon-DUSNE-Pb.ico`,
-      // todo: loadingGif
-      setupIcon: `${ASSET_DIR}/icon.ico`,
-      description: STRINGS.description,
-      exe: `${STRINGS.execName}.exe`,
-      setupExe: `${STRINGS.execName}-setup.exe`,
-      copyright: "Copyright (C) 2025 Revolt Platforms LTD",
-    }),
-    new MakerZIP({}),
-    ...[
-      new MakerFlatpak({
-        options: {
-          id: "chat.stoat.stoat-desktop",
-          description: STRINGS.description,
-          productName: STRINGS.name,
-          productDescription: STRINGS.description,
-          runtimeVersion: "21.08",
-          icon: `${ASSET_DIR}/icon.png`,
-          categories: ["Network"],
-          modules: [
-            // use the latest zypak -- Electron sandboxing for Flatpak
-            {
-              name: "zypak",
-              sources: [
-                {
-                  type: "git",
-                  url: "https://github.com/refi64/zypak",
-                  tag: "v2025.09",
-                },
-              ],
-            },
-          ],
-          finishArgs: [
-            // default arguments found by running
-            // DEBUG=electron-installer-flatpak* pnpm make
-            "--socket=x11",
-            "--share=ipc",
-            "--device=dri",
-            "--socket=pulseaudio",
-            "--filesystem=home",
-            "--env=TMPDIR=/var/tmp",
-            "--share=network",
-            "--talk-name=org.freedesktop.Notifications",
-            // add Unity talk name for badges
-            "--talk-name=com.canonical.Unity",
-          ],
-          // files: [
-          //   // is this necessary?
-          //   // https://stackoverflow.com/q/79745700
-          //   ...[16, 32, 64, 128, 256, 512].map(
-          //     (size) =>
-          //       [
-          //         `assets/desktop/hicolor/${size}x${size}.png`,
-          //         `/app/share/icons/hicolor/${size}x${size}/apps/chat.stoat.stoat-desktop.png`,
-          //       ] as [string, string],
-          //   ),
-          //   [
-          //     `assets/desktop/icon.svg`,
-          //     `/app/share/icons/hicolor/scalable/apps/chat.stoat.stoat-desktop.svg`,
-          //   ] as [string, string],
-          // ],
-          files: [],
-        } as MakerFlatpakOptionsConfig,
-        /* as Omit<
-        MakerFlatpakOptionsConfig,
-        "files"
-      > */
-      }),
-    ].slice(0, 0), // disable Flatpak build
-    new MakerDeb({
-      options: {
-        productName: STRINGS.name,
-        productDescription: STRINGS.description,
-        categories: ["Network"],
-        icon: `${ASSET_DIR}/icon.png`,
-      },
-    }),
-  ],
+  makers,
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
